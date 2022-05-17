f00d733578e793ddba4259b2f8943bd3c61499e3
Nguyen Thuong Hai <58414694+kmille36@users.noreply.github.com>
2022-05-17 23:55:56 +0700

    Update thiet-lap-v2ray.sh

--- thiet-lap-v2ray.sh
+++ thiet-lap-v2ray.sh
@@ -10,11 +10,7 @@ if [ $dist = "CentOS" ] ; then
 	sudo yum update -y
         clear
 	echo" Đang cài đặt phần mềm cần thiết"
-	sudo yum install -y yum-utils
-	sudo yum-config-manager \
-          --add-repo \
-          https://download.docker.com/linux/centos/docker-ce.repo
-	sudo yum -y install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+        sudo yum install -y docker docker.io
 	sudo systemctl start docker
         clear
         echo "Đang tắt firewall"
