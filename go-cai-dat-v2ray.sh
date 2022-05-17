094166f76f2b71efee6e282bf879705e464fd853
Nguyen Thuong Hai <58414694+kmille36@users.noreply.github.com>
2022-05-17 22:44:24 +0700

    Update go-cai-dat-v2ray.sh

--- go-cai-dat-v2ray.sh
+++ go-cai-dat-v2ray.sh
@@ -6,10 +6,10 @@ sudo docker kill v2ray
 echo "Đang nhận diện distro linux"
 dist=$(hostnamectl | egrep "Operating System" | cut -f2 -d":" | cut -f2 -d " ")
 if [ $dist = "CentOS" ] ; then
-	      echo "Bạn đang sử dụng CentOS"
-	      sleep 1
-	      echo" Đang gỡ cài đặt phần mềm cần thiết"
-	      sudo yum remove -y docker docker.io
+	echo "Bạn đang sử dụng CentOS"
+	sleep 1
+        echo" Đang gỡ cài đặt phần mềm cần thiết"
+        sudo yum remove -y docker docker.io
         clear
         echo "Đang bật firewall"
         sudo systemctl start firewalld
@@ -19,9 +19,9 @@ elif [ $dist = "Ubuntu" -o $dist = "Debian" ] ; then
       	echo "Bạn đang sử dụng Ubuntu"
       	sleep 1
       	echo "Đang gỡ đặt phần mềm cần thiết"
-	      sudo apt remove -y docker docker.io
+	sudo apt remove -y docker docker.io
         clear
-	      echo "Đang bật firewall"
+	echo "Đang bật firewall"
         sudo ufw enable
         clear
 fi
