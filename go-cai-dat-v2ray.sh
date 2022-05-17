1fd147f3931291ba4d9ccec05ebe90efcc71fa5d
Nguyen Thuong Hai <58414694+kmille36@users.noreply.github.com>
2022-05-17 22:41:06 +0700

    Create go-cai-dat-v2ray.sh

---
+++ go-cai-dat-v2ray.sh
@@ -0,0 +1,27 @@
+echo "Script by fb.com/thuong.hai.581"
+echo "Hỗ trợ: Ubuntu/Centos"
+echo "Đang dừng v2ray server"
+sudo docker kill v2ray
+echo "Đang nhận diện distro linux"
+dist=$(hostnamectl | egrep "Operating System" | cut -f2 -d":" | cut -f2 -d " ")
+if [ $dist = "CentOS" ] ; then
+	      echo "Bạn đang sử dụng CentOS"
+	      sleep 1
+	      echo" Đang gỡ cài đặt phần mềm cần thiết"
+	      sudo yum remove -y docker docker.io
+        clear
+        echo "Đang bật firewall"
+        sudo systemctl start firewalld
+        sudo systemctl enable firewalld
+        clear
+elif [ $dist = "Ubuntu" -o $dist = "Debian" ] ; then
+      	echo "Bạn đang sử dụng Ubuntu"
+      	sleep 1
+      	echo "Đang gỡ đặt phần mềm cần thiết"
+	      sudo apt remove -y docker docker.io
+        clear
+	      echo "Đang bật firewall"
+        sudo ufw enable
+        clear
+fi
+echo "Xong!!!"
